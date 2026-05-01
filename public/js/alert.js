e4e5df54cbe2c9311ac96b29192c952586c24637
Howard J <enkru2000@hotmail.com>
2026-05-01 17:00:54 +1200

    working in progress

--- public/js/alert.js
+++ public/js/alert.js
@@ -1,6 +1,6 @@
 const alert = (level, text) => {
-	$('#alert').empty();
+	$('#alert, #alert-mobile').empty();
 	const html = `<div class="uk-alert-${level}" uk-alert><a class="uk-alert-close" uk-close></a><p>${text}</p></div>`;
-	$('#alert').append(html);
+	$('#alert, #alert-mobile').append(html);
 	$("html, body").animate({ scrollTop: 0 });
 };
