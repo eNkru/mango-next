4c81d29f2e4105422ecfae002a06412b713c6fe5
Howard J <enkru2000@hotmail.com>
2026-05-02 21:03:52 +1200

    complete the ui refining

--- public/js/search.js
+++ public/js/search.js
@@ -1,7 +1,6 @@
 $(function(){
 	$('.uk-search-input').keyup(function(){
 		var input = $('.uk-search-input').val();
-		$('.topbar-search-input').val(input);
 		if (typeof filterPageItems === 'function') filterPageItems(input);
 	});
 });
