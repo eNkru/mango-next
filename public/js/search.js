25c70e309580a8385e112c2eb6046a692b1f1709
Howard J <enkru2000@hotmail.com>
2026-05-02 16:01:40 +1200

    in progress

--- public/js/search.js
+++ public/js/search.js
@@ -1,39 +1,7 @@
 $(function(){
-	var filter = [];
-	var result = [];
-	var $noResults = $('#no-results');
-	$('.uk-card-title').each(function(){
-		filter.push($(this).text());
-	});
 	$('.uk-search-input').keyup(function(){
 		var input = $('.uk-search-input').val();
-		var regex = new RegExp(input, 'i');
-
-		if (input === '') {
-			$('.item').each(function(){
-				$(this).removeAttr('hidden');
-			});
-			$noResults.hide();
-		}
-		else {
-			var visibleCount = 0;
-			filter.forEach(function(text, i){
-				result[i] = text.match(regex);
-			});
-			$('.item').each(function(i){
-				if (result[i]) {
-					$(this).removeAttr('hidden');
-					visibleCount++;
-				}
-				else {
-					$(this).attr('hidden', '');
-				}
-			});
-			if (visibleCount === 0 && $noResults.length) {
-				$noResults.show();
-			} else {
-				$noResults.hide();
-			}
-		}
+		$('.topbar-search-input').val(input);
+		if (typeof filterPageItems === 'function') filterPageItems(input);
 	});
 });
